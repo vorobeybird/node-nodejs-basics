57748c55913a97c669f1ce9993bb8a33ba75ddbf
Kanstantsin Varabei <vorobeybird@gmail.com>
2022-06-02 19:15:01 +0300

    fix : fixed formatting

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,21 +1,23 @@
 import fs from "fs";
 
-export const rename = async (currentName , targetName) => {
-    // Write your code here 
-    try {
+export const rename = async (currentName, targetName) => {
+  // Write your code here
+  try {
+    await fs.access("./files/" + targetName, (err) => {
+      if (!err) throw "FS operation failed";
+    });
 
-        await fs.access('./files/' + targetName, (err) => {
-            if (!err) throw 'FS operation failed';
-        })
-
-        await fs.rename('./files/' + currentName, './files/' + targetName, err => {
-            if (err) {
-                throw 'FS operation failed';
-            }
-            console.log('FS operation succeed')
-          });
-    } catch ( err ) {
-        console.error(err)
-    }
-
+    await fs.rename(
+      "./files/" + currentName,
+      "./files/" + targetName,
+      (err) => {
+        if (err) {
+          throw "FS operation failed";
+        }
+        console.log("FS operation succeed");
+      }
+    );
+  } catch (err) {
+    console.error("FS operation failed");
+  }
 };
