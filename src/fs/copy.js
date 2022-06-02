1ba6a6adb89b3b5ac0d0450682adec709d645da1
Kostya Vorobey <vorobeybird@gmail.com>
2022-06-02 12:04:21 +0300

    feat: add basic file creation

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,34 @@
-export const copy = async () => {
-    // Write your code here 
+import fs from "fs";
+import path from "path";
+
+export const copy = async (selectedDir, copyDir) => {
+  // Write your code here
+  await fs.access(copyDir, async function (error) {
+    try {
+      if (error) {
+        await fs.mkdir(copyDir, { recursive: true }, (err) => {
+          if (err) throw err;
+        });
+
+        await fs.readdir(selectedDir, (err, files) => {
+          if (err) {
+            throw err;
+          }
+          for (let file in files) {
+            fs.copyFile(
+              path.join("./", selectedDir, "/", files[file]),
+              path.join("./", copyDir, "/", files[file]),
+              (err) => {
+                if (err) throw err;
+              }
+            );
+          }
+        });
+      } else {
+        throw "Operation Failed";
+      }
+    } catch (e) {
+      console.log("error: ", e);
+    }
+  });
 };
