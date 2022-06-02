744efaf106f6728253ad6e23496dfd91eef9cda8
Kanstantsin Varabei <vorobeybird@gmail.com>
2022-06-02 19:21:37 +0300

    feat :  add basic remove function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,12 @@
-export const remove = async () => {
+import fs from "fs";
+import path from "path";
+
+export const remove = async (fileToRemove) => {
     // Write your code here 
+    const filePath = path.join('./files/' + fileToRemove);
+    console.log(filePath);
+    fs.unlink(filePath, (err) => {
+        if (err) throw "FS operation failed";
+        console.log('path/file.txt was deleted');
+      });
 };
