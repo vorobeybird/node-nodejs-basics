7da9dac62b313225281c7e4bcf7324537e8acd18
Kostya Vorobey <vorobeybird@gmail.com>
2022-06-02 12:04:59 +0300

    feat: add basic copy function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,20 @@
-export const create = async () => {
-    // Write your code here 
+import fs from "fs";
+import path from "path";
+
+export const create = async (fileName) => {
+  const filePath = path.join("./files/", fileName);
+
+  await fs.access(filePath, function (error) {
+    try {
+      if (error) {
+        fs.writeFile(filePath, "I am fresh and young", { flag: "wx" }, () =>
+          console.log("File created")
+        );
+      } else {
+        throw "FS operation failed";
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  });
 };
