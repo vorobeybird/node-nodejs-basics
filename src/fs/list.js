274f580ad1f4b56fdf7d0ea558baf3f4ee1e41ab
Kanstantsin Varabei <vorobeybird@gmail.com>
2022-06-03 10:04:10 +0300

    feat: add basic function for printing filenames

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,18 @@
+import fs from 'fs';
+
 export const list = async () => {
-    // Write your code here 
+    // Write your code here
+      
+    try {
+        fs.readdir('./files', (err, files) => {
+            if (err) {
+                throw "FS operation failed";
+            }
+            console.log(files);
+        })
+
+    } catch (e) {
+        throw "FS operation failed";
+    }
+
 };
