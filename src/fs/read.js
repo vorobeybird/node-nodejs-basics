99b048b0f7238909c1d3f65dfc541a551fdbc0e0
Kanstantsin Varabei <vorobeybird@gmail.com>
2022-06-03 10:15:47 +0300

    feat: add basic file read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,29 @@
+import fs from "fs";
+
 export const read = async () => {
-    // Write your code here 
+  // Write your code here\
+  const filePath = "./files/fileToRead.txt";
+
+  fs.open(filePath, "r", (err, fd) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        throw "FS operation failed";
+      }
+
+      throw err;
+    }
+
+    try {
+      fs.readFile(filePath, "utf8", (error, data) => {
+        if (error) {
+          throw "FS operation failed";
+        }
+        console.log(data);
+      });
+    } finally {
+      fs.close(fd, (err) => {
+        if (err) throw err;
+      });
+    }
+  });
 };
