9607e858b847d775146bd201b109b24470b249f3
Kanstantsin Varabei <vorobeybird@gmail.com>
2022-06-05 22:10:27 +0300

    feat: add basic hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,17 @@
+import crypto from "crypto";
+import fs from "fs";
+
 export const calculateHash = async () => {
-    // Write your code here 
+  // Write your code here
+  const hashSum = crypto.createHash("sha256");
+  const input = fs.createReadStream("./files/fileToCalculateHashFor.txt");
+  input.on("readable", () => {
+    const data = input.read();
+    if (data) hashSum.update(data);
+    else {
+      console.log(`${hashSum.digest("hex")}`);
+    }
+  });
 };
+
+calculateHash();
