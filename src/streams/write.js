1c51e7dd4b7a38149c5d46fee99b76e7cc4b37d4
Kanstantsin Varabei <vorobeybird@gmail.com>
2022-06-05 21:56:48 +0300

    feat: add basic write stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,14 @@
+import fs from 'fs';
+
+const writableStream = fs.createWriteStream('./files/fileToWrite.txt');
+
 export const write = async () => {
-    // Write your code here 
+    process.stdin.on('data', chunk => {
+        writableStream.write(chunk);
+    })
+    process.stdin.on('end', ()=> {
+        process.exit();
+    })
 };
+
+write();
