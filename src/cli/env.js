c8d071b06cc50b94246dae1da7f5f584b11a96c4
Kanstantsin Varabei <vorobeybird@gmail.com>
2022-06-05 12:53:54 +0300

    feat: add basic env parser

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,11 @@
+
 export const parseEnv = () => {
-    // Write your code here 
+
+    const asArray = Object.entries(process.env);
+
+    const filtered = Object.fromEntries(asArray.filter(([key, value]) => key.slice(0,4) === 'RSS_'));
+
+    for ( let key in filtered) {
+        process.stdout.write(`${key}=${filtered[key]}; `)
+    }
 };
