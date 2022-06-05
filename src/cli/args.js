d337f990f3602aa3ecf60730038de631992c03ad
Kanstantsin Varabei <vorobeybird@gmail.com>
2022-06-05 13:47:39 +0300

    feat: add basic parse args function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,4 +1,12 @@
 export const parseArgs = () => {
-    // Write your code here \
-    //init code
+  // Write your code here \
+  //init code
+  const doubleDashCollection = [];
+  const args = process.argv.slice(2);
+  for (let i in args) {
+    if (args[i].slice(0, 2) === "--" && args[+i + 1].slice(0, 2) !== "--") {
+      doubleDashCollection.push(`${[args[i].slice(2)]} is ${args[+i + 1]}`);
+    }
+  }
+  console.log(doubleDashCollection.join(", "));
 };
