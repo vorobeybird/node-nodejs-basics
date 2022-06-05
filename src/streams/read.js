e65952c0a334fe0216dfe59da45b7e3c2972997b
Kanstantsin Varabei <vorobeybird@gmail.com>
2022-06-05 17:11:10 +0300

    feat: add readable streaming function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,17 @@
+import fs from "fs";
+
 export const read = async () => {
-    // Write your code here 
+  // Write your code here
+  const rr = fs.createReadStream("./files/fileToRead.txt");
+  rr.on("readable", () => {
+    let streamLine = rr.read();
+    if (streamLine) {
+      process.stdout.write(streamLine);
+    }
+  });
+  rr.on("end", () => {
+    console.log("\nend");
+  });
 };
+
+read();
